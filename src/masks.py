63f6398738290e69aac2f92806852aa70e52a2fd
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-06-19 19:13:08 +0300

    function mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -1,53 +1,40 @@
-'''
-Работа с зависимостями и виртуальным окружением ведется через Poetry.
-Есть файл
-.flake8
- с конфигурацией для линтера Flake8.
-В
-pyproject.toml
- есть конфигурации для
-black
-,
-isort
-,
-mypy
-.
-Функции соответствуют PEP 8, имеют аннотации типов, корректный нейминг и docstring.
-'''
-
-
-def get_mask_card_number(card_number: int) -> str:
+def mask_account_card(card_or_account: str) -> str:
     """
-    Функция принимает на вход номер карты и возвращает ее маску - видны первые 6 и последние 4 цифр
+    Функция принимает на вход информацию о карте или счете и возвращает зашифрованную информацию
     """
-    list_card_number: list = list(str(card_number))
-    for index in range(len(list_card_number)):
+    list_card_or_account: list = card_or_account.split()
 
-        if 6 <= index < len(list_card_number) - 4:
-            list_card_number[index] = "*"
+    #Обработка карты
+    if list_card_or_account[0] in ["Visa", "MasterCard", "Maestro"]:
+        #Шифруем номер карты
+        list_card_or_account[-1] = f"{list_card_or_account[-1][:6]}******{list_card_or_account[-1][-4:]}"
 
-    counter = 0
-    for index in range(1, len(list_card_number)):
-        if index % 4 == 0:
-            list_card_number.insert(index + counter, " ")
-            counter += 1
-
-    return "".join(list_card_number)
-
-
-def get_mask_account(account: int) -> str:
-    """
-    Функция принимает на вход номер счета и возвращает его маску - видны последние 4 цифр
-    """
-
-    list_account: list = list(str(account))
-    for index in range(len(list_account)):
-
-        if index < len(list_account) - 4:
-            list_account[index] = "*"
-    return "".join(list_account)[-6:]
+        #Оборачиваем номер карты в список для перебора и вставки пробелов
+        list_number_card_account = list(list_card_or_account[-1])
+        counter = 0
+        for index in range(1, len(list_number_card_account)):
+            if index % 4 == 0:
+                list_number_card_account.insert(index + counter, " ")
+                counter += 1
+        #Заменяем исходную строку с номером карты на зашифрованную с пробелами
+        else:
+            list_card_or_account[-1] = "".join(list_number_card_account)
+    #Обработка счета
+    elif list_card_or_account[0] == "Счет":
+        list_card_or_account[-1] = f"**{list_card_or_account[-1][-2:]}"
+    #Прочие случаи
+    else:
+        return "Не верно введенный номер счета или карты"
+    return " ".join(list_card_or_account)
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number(7000792289606361))
-    print(get_mask_account(73654108430135874305))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Счет 35383033474447895560"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Карта"))
--- tests/main.py
+++ tests/main.py
@@ -1,5 +1,12 @@
 import src.masks
 
 
-print(src.masks.get_mask_card_number(7000792289606361))
-print(src.masks.get_mask_account(73654108430135874305))
+print(src.masks.mask_account_card("Maestro 1596837868705199"))
+print(src.masks.mask_account_card("Счет 64686473678894779589"))
+print(src.masks.mask_account_card("MasterCard 7158300734726758"))
+print(src.masks.mask_account_card("Счет 35383033474447895560"))
+print(src.masks.mask_account_card("Visa Classic 6831982476737658"))
+print(src.masks.mask_account_card("Visa Platinum 8990922113665229"))
+print(src.masks.mask_account_card("Visa Gold 5999414228426353"))
+print(src.masks.mask_account_card("Счет 73654108430135874305"))
+print(src.masks.mask_account_card("Карта"))
