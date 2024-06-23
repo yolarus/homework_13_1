562c669885188714ce2378e27ca1ae606f778ae2
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-06-23 09:03:32 +0300

    add func filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def filter_by_state(id_info: List[dict], state: str = "EXECUTED") -> List[dict]:
+    """
+    Функция фильтрует список словарей id с по ключу state
+    """
+    filter_id_info = []
+
+    for element in id_info:
+        if element.get("state") == state:
+            filter_id_info.append(element)
+
+    return filter_id_info
+
+
+if __name__ == "__main__":
+    tests_info = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+    print(filter_by_state(tests_info))
+    print(filter_by_state(tests_info, "CANCELED"))
