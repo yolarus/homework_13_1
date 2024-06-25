55e90f5c4aae43e8347ed2388bed16e32c716162
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-06-26 00:22:29 +0300

    small fixes in test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,16 +3,6 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
-my_list = ["Maestro 1596837868705199",
-           "Счет 64686473678894779589",
-           "MasterCard 7158300734726758",
-           "Счет 35383033474447895560",
-           "Visa Classic 6831982476737658",
-           "Visa Platinum 8990922113665229",
-           "Visa Gold 5999414228426353",
-           "Счет 73654108430135874305"
-           ]
-
 
 @pytest.mark.parametrize("card_info, expected", [("Maestro 1596837868705199".split(),
                                                   "Maestro 1596 83** **** 5199"),
