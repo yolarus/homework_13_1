f647acb82f86912411c084106875492b733b9595
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-07-14 19:00:26 +0300

    add test_external_api.py

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 from unittest.mock import patch, mock_open
 from typing import Any
 
---
+++ tests/test_external_api.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+from typing import Any
+
+
+from src.external_api import get_sum_of_transaction
+
+
+@patch("requests.get")
+def test_get_sum_of_transaction_usd(mock_get: Any) -> None:
+    mock_get.return_value.json.return_value = {"result": 100}
+    assert get_sum_of_transaction({"operationAmount": {"currency": {"code": "USD"}, "amount": 1}}) == 100
+    mock_get.assert_called_once()
+
+
+def test_get_sum_of_transaction_rub() -> None:
+    assert get_sum_of_transaction({"operationAmount": {"currency": {"code": "RUB"}, "amount": 100}}) == 100
